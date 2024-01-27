a9c4206115f6254b1f5e1aa0809ee1bd4d2333ad
zkanga <zkanga@gmail.com>
2024-01-26 22:44:43 -0800

    Test version created.

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ TYPE_2_EXPECT_MAP = {
 }
 
 DELIM = '|'
+output_file = "results.txt"
 
 
 def parse_config(config_path):
-    with open(config_path, 'r') as f:
-        config = json.load(f)
+    try:
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+    except Exception as e:
+        print(f"ERROR: reading config file {config_path}.\n{e}")
+        raise
     expectations = []
     # TODO: confirm there aren't multiple suites
     GreatestExpectations.suite_name = config["SUITE"]["NAME"]
@@ -25,20 +30,24 @@ def parse_config(config_path):
 
 
 def read_csv(input_file, delimiter, expectations):
-    with open(input_file, 'r', newline='') as csv_file:
-        # Create a CSV reader object
-        csv_reader = csv.reader(csv_file, delimiter=delimiter)
-        header = next(csv_reader)
-        for expectation in expectations:
-            expectation.field_location = header.index(expectation.field_name)
-        # Iterate over each row in the CSV file
-        for line_number, row in enumerate(csv_reader, 1):
-            # print(line_number, row)
+    try:
+        with open(input_file, 'r', newline='') as csv_file:
+            # Create a CSV reader object
+            csv_reader = csv.reader(csv_file, delimiter=delimiter)
+            header = next(csv_reader)
             for expectation in expectations:
-                expectation.validate(line_number, row)
+                expectation.field_location = header.index(expectation.field_name)
+            # Iterate over each row in the CSV file
+            for line_number, row in enumerate(csv_reader, 1):
+                # print(line_number, row)
+                for expectation in expectations:
+                    expectation.validate(line_number, row)
+    except FileNotFoundError:
+        print(f"File '{input_file}' not found.")
+        raise
 
 
-def prepare_output(expects):
+def output_data(expects, out_file):
     out = ""
     all_pass = True
     for expect in expects:
@@ -51,15 +60,20 @@ def prepare_output(expects):
     else:
         message = "FAILURE"
     out = f"{GreatestExpectations.suite_name} data expectations suite results: {message}\n{out}"
-    return out
+    try:
+        with open(out_file, "w") as text_file:
+            text_file.write(out)
+    except Exception as e:
+        print(f"Error writing to file {out_file}.\n{e}")
+        raise
+    # return out
 
 
 def validator(config, input_file_name):
     expects = parse_config(config)
     # Pass by reference - expects updated in read_csv
     read_csv(input_file_name, DELIM, expects)
-    output = prepare_output(expects)
-    print(output)
+    output_data(expects, output_file)
 
 
 if __name__ == "__main__":
